4d320a5336abaf8864b497d002a543d78733c6a8
Mega <148019505+megavillain@users.noreply.github.com>
2024-03-18 19:37:36 -0700

    Init Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+import asyncio
+from discord import Intents
+import ollama
+
+#llm = 'dolphin-mistral:latest'
+llm = 'gdisney/mistral-uncensored:latest'
+prompt = ''
+
+# Define intents
+intents = Intents.all()
+intents.messages = True  # Enable message events
+
+# Discord client with intents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:  # Ignore messages sent by the bot itself
+        return
+
+    # Check if the bot was mentioned
+    if client.user.mentioned_in(message):
+        # Check if the message is sent in the specified channel
+        if message.channel.name == 'general':
+            # Generate response using Ollama
+            reply = ollama.chat(model=llm, messages=[
+                {
+                    'role': 'user',
+                    'content': f"Don't put quotes around your replies. You are a funny Discord bot named Henchbot, join in the conversation and craft a funny reply to {message.author.display_name} who wrote: {message.clean_content}"
+                },
+            ])
+            #print(message.author.display_name)
+            #print(reply['message']['content'])
+            # Send the response to the same channel
+            await message.channel.send(reply['message']['content'])
+
+
+# Read bot token from file
+with open("henchbot_token.txt", "r") as token_file:
+    TOKEN = token_file.read().strip()
+
+# Run the Discord bot
+client.run(TOKEN)
